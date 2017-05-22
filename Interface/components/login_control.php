65611bbdecaac85a6e34c1c40a8ef807477c399c
Rodrigo Sousa <rodrigo.sousa@ua.pt>
2017-05-22 17:58:06 +0100

    Form de registo e validação adicionadas

--- Interface/components/login_control.php
+++ Interface/components/login_control.php
@@ -1,5 +1,19 @@
 <?php
+session_start();
 
 echo ($_POST["inputUsername"] . " " . $_POST["inputPassword"]);
 
-var_dump($_POST);
+$sql_server = "localhost";
+$sql_username = "root";
+$sql_password = "";
+
+$sql_connection = new mysqli($sql_server, $sql_username, $sql_password, "deca_16l4_69");
+
+if (!$sql_connection) {
+
+	die("Connection failure: " . mysqli_connect_error());
+
+}
+
+$inputUsername = $_POST["inputUsername"];
+$inputPassword = $_POST["inputPassword"];
